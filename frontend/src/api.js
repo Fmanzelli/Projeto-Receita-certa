f811302b30573771bae0df2dfd0563c12dcdce26
Felipe Manzelli <felipe_manzelli@hotmail.com>
2026-05-16 13:57:31 -0300

    fix: atualiza URL de producao para apontar para o novo backend na Railway

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // O Vite sabe automaticamente quando está na Vercel (PROD) ou no seu PC (DEV)
 const backendUrl = import.meta.env.PROD
-  ? 'https://projeto-receita-certa.onrender.com' // <-- TROQUE PELA SUA URL REAL DA RENDER AQUI!!!
+  ? 'https://projeto-receita-certa-production.up.railway.app' // <-- TROQUE PELA SUA URL REAL DA RENDER AQUI!!!
   : 'http://localhost:3000';
 
 const api = axios.create({
